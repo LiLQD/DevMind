dcb095373477317b9f7a90ef9229aee454d161cf
LiLQD <luuquangdung1511@gmail.com>
2026-08-21 16:15:16 +0700

    fix: remove global.crypto assignment

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import mongoose from "mongoose";
-import crypto from "crypto";
-
-// Make crypto globally available
-global.crypto = crypto;
 
 dotenv.config();
 
